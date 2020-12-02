f9f0cdfb9c19d77006f2825be72344862f791605
Saffet Turkoglu <saffet.turkoglu@siemens.com>
2020-12-02 12:19:42 +0300

    fix

--- src/app/file-upload.service.ts
+++ src/app/file-upload.service.ts
@@ -14,7 +14,7 @@ export class FileUploadService {
     const formData: any = new FormData();
     formData.append('name', name);
     formData.append('file', uploadingFile);
-    return this.http.put('https://saffet-demo.s3.amazonaws.com/testfile.txt?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIATVKLKNDP7MTLBZ6L%2F20201201%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20201201T083904Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=394fc2fdb161334b4c083340532eb6805ba76b1ac902864da952c9a7625495ab', formData, {
+    return this.http.put('presignedUrl', formData, {
       reportProgress: true,
       observe: 'events'
     }).pipe(
